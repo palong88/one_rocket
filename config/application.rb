06333a0292f7d3885d7720ae26b5359597b5a83f
palong88 <patrick@plongmail.info>
2016-04-28 16:23:46 +0100

    bundle install, all ready to push to heroku 18

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,15 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  class_attr_index = html_tag.index 'class="'
 
+  if class_attr_index
+    html_tag.insert class_attr_index+7, 'error '
+  else
+    html_tag.insert html_tag.index('>'), ' class="error"'
+  end
+end
 
 module Onerocketing
   class Application < Rails::Application
