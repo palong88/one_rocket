6f991959a9e9d53a11c0d2cca08b4347ed25e7ef
palong88 <patrick@plongmail.info>
2016-05-01 12:44:50 +0100

    bundle install, all ready to push to heroku 20

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,11 +2,10 @@
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
+//= require jquery-ui
 //= require materialize-sprockets
 //= require turbolinks
 //= require_tree .
 
 
-
-
-
+   
