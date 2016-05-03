9bfcfe7da840b801b2af36a691d2f93b9ca55565
palong88 <patrick@plongmail.info>
2016-05-03 17:14:28 +0100

    bundle install, all ready to push to heroku 22.04

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,17 @@
 //= require_tree .
 
 
+  
+  $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
+        
+          $(document).ready(function(){
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+  });
+        
