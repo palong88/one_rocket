47a450b60377cad8cb2d2251a495f8c3f3976d96
palong88 <patrick@plongmail.info>
2016-05-15 22:27:15 +0100

    Order of Taks tweeked debug13

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512203330) do
+ActiveRecord::Schema.define(version: 20160515212333) do
 
   create_table "admin_tasks", force: :cascade do |t|
     t.string   "title"
